40cbc1f16105dffca6c6f55153fa2ab3df975d23
imrulhassanemon <imrulhassan989@gmail.com>
2024-10-14 11:09:13 +0600

    added arrow.js, logical-operator, scope.js

---
+++ arrow.js
@@ -0,0 +1,27 @@
+const func = () =>{
+    return "ki chai...?"
+}
+
+
+const squ = (x) => {
+    return x**2
+}
+
+// console.log(squ(5));
+
+const add = (x, y) => x + y;
+// console.log(add(8,9));
+
+// default parameter in function 
+
+function def (x = 0, y = 0) {
+    return x + y;
+}
+console.log(def(3))
+
+
+const defult = (x = 0, y = 0) => x + y;
+console.log(defult(5, 1))
+
+
+
---
+++ scope.js
@@ -0,0 +1,18 @@
+// const x = 5;
+
+console.log(5)
+
+if(true){
+    console.log(5)
+}
+
+function show(){
+    console.log('function', x)
+}
+show()
+
+
+{
+    var x = 10
+    console.log(x)
+}
